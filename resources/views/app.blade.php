541b72c36a01c95023e16a68e878ea396742c927
Yhann Daniel <yhanndaniel@gmail.com>
2021-01-31 15:18:11 -0300

    add inertia in backend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Crud Laravel Vue</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
   </head>
   <body>
